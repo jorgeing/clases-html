5fc80b02b67b514d6b0fa530c6c35533679a32d4
jorgeing <84467428+jorgeing@users.noreply.github.com>
2023-11-28 18:39:41 +0000

    configurar estilos

--- controlador-temperatura-demo/controlador.js
+++ controlador-temperatura-demo/controlador.js
@@ -1,6 +1,48 @@
+//FUNCIONES CONEXION
+async function connect() {
+    try {
+        const port = await navigator.serial.requestPort();
+        await port.open({ baudrate: 9600 }); // Ajusta la velocidad de baudios según la configuración de tu micro:bit
 
-deslizador = document.getElementById("simuladorTemperatura");
-textoTemperatura = document.getElementById("temperatura");
-deslizador.addEventListener("input", (event)=>{
-    textoTemperatura.textContent = event.target.value 
+        // Escuchar eventos de recepción de datos
+        const reader = port.readable.getReader();
+        console.log("Conectado!")
+        return reader;
+    } catch (error) {
+        console.error("Error de conexión:", error);
+        return null;
+    }
+}
+
+//FUNCIONES CONTROL
+function controlVentilador() {
+    let temperaturaActual = document.getElementById("simuladorTemperatura").value;
+    let temperaturaMaxima = document.getElementById("temperaturaMaxima").value;
+    console.log({temperaturaActual,temperaturaMaxima});
+    const textoVentilador = document.getElementById("ventilador");
+    const iconoVentilador = document.getElementById("textoVentilador");
+
+    if(temperaturaActual>temperaturaMaxima)
+    {
+        textoVentilador.textContent="Encendido";
+        iconoVentilador.style.color="green";
+    }
+    else
+    {
+        textoVentilador.textContent="Apagado";
+        iconoVentilador.style.color="red";
+    }
+
+}
+
+//Ejecutar pagina
+let deslizador = document.getElementById("simuladorTemperatura");
+let textoTemperatura = document.getElementById("temperatura");
+deslizador.addEventListener("input", (event) => {
+    textoTemperatura.textContent = event.target.value
 });
+
+//document.onclick = async () => {
+//    lector = await connect();
+//}
+    setInterval(controlVentilador,1000);
